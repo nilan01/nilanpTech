48e2d8b167943dfc6b195e45ec7c50c2b0e87710
nilan01 <nilanpatel33@hotmail.com>
2020-03-11 17:47:22 -0400

    mobile changes

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
   render() {
     return(
     <div className="App">
-    <Navbar sticky="top">
+    <Navbar>
       <Navbar.Collapse className="justify-content-end">
         <Nav></Nav>
       </Navbar.Collapse>
